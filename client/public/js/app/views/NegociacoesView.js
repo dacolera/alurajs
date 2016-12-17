a4b4563d9c9aa06f70f71a3bc187c700764887d3
dacolera <dacolera360@gmail.com>
2016-12-17 00:03:11 -0200

    [dacolera] refatora usando arrow function e cria td mostrando a totalizacao no footer da tabela

--- client/public/js/app/views/NegociacoesView.js
+++ client/public/js/app/views/NegociacoesView.js
@@ -7,33 +7,32 @@ class NegociacoesView {
 	_template(model) {
 		return `
 			<table class="table table-hover table-bordered">
-		        <thead>
-		            <tr>
-		                <th>DATA</th>
-		                <th>QUANTIDADE</th>
-		                <th>VALOR</th>
-		                <th>VOLUME</th>
-		            </tr>
-		        </thead>
-		        
-		        <tbody>
-		        	${model.negociacoes.map(
-		        		n => {
-		        			return `
-		        				<tr>
-		        					<td>${DateHelper.formataData(n.data)}</td>
-		        					<td>${n.quantidade}</td>
-		        					<td>${n.valor}</td>
-		        					<td>${n.volume}</td>
-		        				</tr>
-		        			`;
-		        		}
-		        	).join("")}
-		        </tbody>
-		        
-		        <tfoot>
-		        </tfoot>
-		    </table>
+				<thead>
+					<tr>
+						<th>DATA</th>
+						<th>QUANTIDADE</th>
+						<th>VALOR</th>
+						<th>VOLUME</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					${model.negociacoes.map(n =>
+					`
+						<tr>
+							<td>${DateHelper.formataData(n.data)}</td>
+							<td>${n.quantidade}</td>
+							<td>${n.valor}</td>
+							<td>${n.volume}</td>
+						</tr>
+					`
+					).join("")}
+				</tbody>
+				<tfoot>
+					<td colspan="3"></td>
+					<td>${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}</td>
+				</tfoot>
+			</table>
 		`;
 	}
 
