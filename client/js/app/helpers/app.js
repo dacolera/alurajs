611d793e1cabab4e765aff4abb85e999f5560c17
dacolera <dacolera360@gmail.com>
2016-12-17 23:35:35 -0200

    [dacolera] implementa a coneccao com IDB usando os patterns module e promise

--- client/js/app/helpers/app.js
+++ client/js/app/helpers/app.js
@@ -1,4 +1,51 @@
 ;(function(document, window, undefined){
-
     window.$ = document.querySelector.bind(document);
 })(document, window);
+
+var ConnectionFactory = (function(){
+
+    var config = {
+        version : 4,
+        dbname : 'aluraframe',
+        stores : [
+            'negociacoes'
+        ],
+        connection : null
+    };
+
+    return class ConnectionFactory {
+
+        constructor() {
+            throw Error('Não é possivel criar uma instância de ConnectionFactory');
+        }
+
+        static getConnection() {
+
+            return new Promise((resolve, reject) => {
+                let openRequest = window.indexedDB.open(config.dbname, config.version);
+
+                openRequest.onupgradeneeded = e => ConnectionFactory._createStore(e.target.result);
+
+                openRequest.onsuccess = e => {
+                    config.connection = null !== config.connection ? config.connection : e.target.result;
+                    resolve(config.connection);
+                }
+
+                openRequest.onerror = e => reject(e.target.error.name);
+            });
+        }
+
+        static _createStore(connection) {
+
+            config.stores.forEach(store => {
+
+                if(connection.objectStoreNames.contains(store)) {
+                    connection.deleteObjectStore(store);
+                }
+
+                connection.createObjectStore(store, { autoIncrement: true });
+            });
+        }
+    }
+
+})();
